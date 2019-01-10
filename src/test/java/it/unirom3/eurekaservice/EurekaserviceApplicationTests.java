1f6fe74ef8a261e2a73a4fb03c15073249573601
gianlorenzo <gianlorenzo.didonato@gmail.com>
2019-01-10 13:33:12 +0100

    Initial commit

---
+++ src/test/java/it/unirom3/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,17 @@
+package it.unirom3.eurekaservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurekaserviceApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
